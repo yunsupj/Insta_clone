86b7fcf7ead4293fa749091dd283ce3be13ac1b0
yunsupj <yunsup.j@gmail.com>
2017-12-13 20:12:49 -0800

    Make image model + FK set_up

--- insta_clone/images/models.py
+++ insta_clone/images/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from insta_clone.users import models as user_models
 
 # Create your models here.
+class TimeStampedModel(models.Model):
+    
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+class Image(TimeStampedModel):
+    
+    """ Image Model """
+
+    file = models.ImageField()
+    location = models.CharField(max_length=140)
+    caption = models.TextField()
+    creator = models.ForeignKey(user_models.User, null=True)
+
+class Comment(TimeStampedModel):
+    
+    """ Comment Model """
+
+    message = models.TextField()
+    creator = models.ForeignKey(user_models.User, null=True)
+    image = models.ForeignKey(Image, null=True)
+
+class Like(TimeStampedModel):
+
+    """ Like Model """
+    
+    creator = models.ForeignKey(user_models.User, null=True)
+    image = models.ForeignKey(Image, null=True)
