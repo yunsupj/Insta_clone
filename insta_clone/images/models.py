d2b5c4564fdd60f84fb843cdcb0a319a45125ef6
yunsupj <yunsup.j@gmail.com>
2017-12-14 23:13:43 -0800

    setup django admin

--- insta_clone/images/models.py
+++ insta_clone/images/models.py
@@ -1,7 +1,9 @@
 from django.db import models
 from insta_clone.users import models as user_models
+from django.utils.encoding import python_2_unicode_compatible
 
-# Create your models here.
+
+@python_2_unicode_compatible
 class TimeStampedModel(models.Model):
     
     created_at = models.DateTimeField(auto_now_add=True)
@@ -10,6 +12,8 @@ class TimeStampedModel(models.Model):
     class Meta:
         abstract = True
 
+
+@python_2_unicode_compatible
 class Image(TimeStampedModel):
     
     """ Image Model """
@@ -19,6 +23,11 @@ class Image(TimeStampedModel):
     caption = models.TextField()
     creator = models.ForeignKey(user_models.User, null=True)
 
+    def __str__(self):
+        return '{} - {}'.format(self.location, self.caption)
+
+
+@python_2_unicode_compatible
 class Comment(TimeStampedModel):
     
     """ Comment Model """
@@ -27,9 +36,17 @@ class Comment(TimeStampedModel):
     creator = models.ForeignKey(user_models.User, null=True)
     image = models.ForeignKey(Image, null=True)
 
+    def __str__(self):
+        return self.message
+
+
+@python_2_unicode_compatible
 class Like(TimeStampedModel):
 
     """ Like Model """
     
     creator = models.ForeignKey(user_models.User, null=True)
     image = models.ForeignKey(Image, null=True)
+
+    def __str__(self):
+        return 'User: {} - Image Caption: {}'.format(self.creator.username, self.image.caption)
